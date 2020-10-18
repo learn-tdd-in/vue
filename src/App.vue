e6204c59aff040bd7b0604530097f55ec4cc0d5c
Josh Justice <me@codingitwrong.com>
2020-10-18 08:12:26 -0400

    Display messages

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
-    <new-message-form />
+    <new-message-form @send="addMessage" />
+    <message-list :messages="messages" />
   </div>
 </template>
 
 <script>
 import NewMessageForm from "./components/NewMessageForm";
+import MessageList from './components/MessageList';
 
 export default {
   name: "App",
   components: {
     NewMessageForm,
+    MessageList
   },
+  data() {
+    return {
+      messages: []
+    };
+  },
+  methods: {
+    addMessage(text) {
+      this.messages.unshift(text);
+    }
+  }
 };
 </script>
